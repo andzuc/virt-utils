c9c230bfc566465508eceab7eb4e04da1da6c62d
Andrea Zuccherelli <andrea.zuccherelli@gmail.com>
2026-02-22 19:53:30 +0100

    virt_lib

---
+++ virt_lib.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -euo pipefail
+
+function virdump() {
+    if ! command -v virsh >/dev/null 2>&1 ; then
+	echo "$0: missing command virsh"
+	exit 1
+    fi
+    if [ -z "$1" ]; then
+	echo "$0: missing VM name"
+	exit 1
+    fi
+    VM_NAME="$(virsh -c qemu:///system list --all --name|grep -i $1)"
+    virsh -c qemu:///system dumpxml "$VM_NAME"
+}
+
+function virget() {
+    if ! command -v xmlstarlet >/dev/null 2>&1 ; then
+	echo "$0: missing command xmlstarlet"
+	exit 1
+    fi
+    if [ -z "$2" ]; then
+	echo "$0: missing xpath"
+	exit 1
+    fi
+    dumpxml $1 \
+    |xmlstarlet sel -t -v "$2"
+}
